1b65ce5fc046f13d3eedb3337ed12ea2f4340380
Hsensor <1203460788@qq.com>
2017-08-10 18:22:42 +0800

    immediate

--- packages/picker/src/picker-slot.vue
+++ packages/picker/src/picker-slot.vue
@@ -435,7 +435,8 @@
           if ((this.mutatingValues[val] !== undefined) && (this.mutatingValues.length >= val + 1)) {
             this.currentValue = this.mutatingValues[val];
           }
-        }
+        },
+        immediate: true
       }
     }
   };
